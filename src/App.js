4dff297549fda1aa85e2c1e134479b5349dbee07
Timothy Stewart <timothystewart6@gmail.com>
2020-09-11 20:00:27 -0500

    chore(init): initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://www.youtube.com/channel/UCOk-gHyjcWZNj3Br4oxwh0A"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Techno Tim on YouTube
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/App.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/Techno Tim/i);
+  expect(linkElement).toBeInTheDocument();
+});
